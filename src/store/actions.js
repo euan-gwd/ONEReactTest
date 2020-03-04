8ffa20d6380b99b06e6b514d9046b4006c3db424
Euan Greenwood <euan@euangreenwood.com>
2020-03-04 02:08:42 +0000

    add lodash

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,65 +1,8 @@
 import * as actionTypes from './constants';
+import _orderBy from 'lodash/orderBy';
 
 const getProducts = () => {
   return fetch('https://j-parre.myshopify.com/products.json').then((res) => res.json());
-};
-
-const compareValues = (key, order = 'asc') => {
-  return function(a, b) {
-    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-      return 0;
-    }
-
-    // const itemA = typeof a[key] === 'string' ? a[key].toUpperCase() : a[key];
-    // const itemB = typeof b[key] === 'string' ? b[key].toUpperCase() : b[key];
-
-    // const itemA = a[key];
-    // const itemB = b[key];
-
-    // let comparison = 0;
-    // if (itemA > itemB) {
-    //   comparison = 1;
-    // } else if (itemA < itemB) {
-    //   comparison = -1;
-    // }
-
-    let comparison = 0;
-    a[key] > b[key] ? (comparison = 1) : (comparison = -1);
-
-    return order === 'desc' ? comparison * -1 : comparison;
-  };
-};
-
-const sortProductsByTitleAscending = () => {
-  return (dispatch, getState) => {
-    const productsClone = [...getState().products];
-    const sortedProducts = productsClone.sort(compareValues(title, 'asc'));
-    dispatch({ type: actionTypes.SORT_PRODUCTS_TITLE_ASC, payload: sortedProducts });
-  };
-};
-
-const sortProductsByTitleDecending = () => {
-  return (dispatch, getState) => {
-    const productsClone = [...getState().products];
-    const sortedProducts = productsClone.sort(compareValues(title, 'desc'));
-    dispatch({ type: actionTypes.SORT_PRODUCTS_TITLE_DESC, payload: sortedProducts });
-  };
-};
-
-const sortProductsByPriceAscending = () => {
-  return (dispatch, getState) => {
-    const productsClone = [...getState().products];
-    const sortedProducts = productsClone.sort(compareValues(price, 'asc'));
-    dispatch({ type: actionTypes.SORT_PRODUCTS_PRICE_ASC, payload: sortedProducts });
-  };
-};
-
-const sortProductsByPriceDecending = () => {
-  return (dispatch, getState) => {
-    const productsClone = [...getState().products];
-    const sortedProducts = productsClone.sort(compareValues(price, 'desc'));
-    dispatch({ type: actionTypes.SORT_PRODUCTS_PRICE_DESC, payload: sortedProducts });
-  };
 };
 
 export const loadProducts = () => {
@@ -72,5 +15,37 @@ export const loadProducts = () => {
       .catch((error) => {
         dispatch({ type: actionTypes.LOAD_PRODUCTS_ERROR, error });
       });
+  };
+};
+
+const sortProductsByTitleAscending = () => {
+  return (dispatch, getState) => {
+    const products = getState().products;
+    const sortedProducts = _orderBy(products, ['title'], ['asc']);
+    dispatch({ type: actionTypes.SORT_PRODUCTS_TITLE_ASC, payload: sortedProducts });
+  };
+};
+
+const sortProductsByTitleDecending = () => {
+  return (dispatch, getState) => {
+    const products = getState().products;
+    const sortedProducts = _orderBy(products, ['title'], ['desc']);
+    dispatch({ type: actionTypes.SORT_PRODUCTS_TITLE_DESC, payload: sortedProducts });
+  };
+};
+
+const sortProductsByPriceAscending = () => {
+  return (dispatch, getState) => {
+    const products = getState().products;
+    const sortedProducts = _orderBy(products, ['price'], ['asc']);
+    dispatch({ type: actionTypes.SORT_PRODUCTS_PRICE_ASC, payload: sortedProducts });
+  };
+};
+
+const sortProductsByPriceDecending = () => {
+  return (dispatch, getState) => {
+    const products = getState().products;
+    const sortedProducts = _orderBy(products, ['price'], ['desc']);
+    dispatch({ type: actionTypes.SORT_PRODUCTS_PRICE_DESC, payload: sortedProducts });
   };
 };
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -4,7 +4,8 @@ import * as actionTypes from './constants';
 const initialState = {
   productsLoading: false,
   productsError: null,
-  products: []
+  products: [],
+  shoppingCart: []
 };
 
 export const reducer = produce((draft, action) => {
@@ -21,6 +22,18 @@ export const reducer = produce((draft, action) => {
     case actionTypes.LOAD_PRODUCTS_ERROR:
       draft.productsLoading = false;
       draft.productsError = action.error;
+      return;
+    case actionTypes.SORT_PRODUCTS_TITLE_ASC:
+      draft.products = action.payload;
+      return;
+    case actionTypes.SORT_PRODUCTS_TITLE_DESC:
+      draft.products = action.payload;
+      return;
+    case actionTypes.SORT_PRODUCTS_PRICE_ASC:
+      draft.products = action.payload;
+      return;
+    case actionTypes.SORT_PRODUCTS_PRICE_DESC:
+      draft.products = action.payload;
       return;
     default:
       return;
