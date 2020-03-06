411b17188e3719cc9b4ce79ae2de8597d46d9115
Euan Greenwood <euan@euangreenwood.com>
2020-03-06 11:56:13 +0000

    add enzyme

--- tests/App-test.js
+++ tests/App-test.js
@@ -1,23 +1,24 @@
-import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import React from 'react';
+import expect from 'expect';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { shallow } from 'enzyme';
 
-import App from 'src/App'
+import App from 'src/components/App';
 
 describe('App component', () => {
-  let node
+  let node;
 
   beforeEach(() => {
-    node = document.createElement('div')
-  })
+    node = document.createElement('div');
+  });
 
   afterEach(() => {
-    unmountComponentAtNode(node)
-  })
+    unmountComponentAtNode(node);
+  });
 
-  it('displays a welcome message', () => {
-    render(<App/>, node, () => {
-      expect(node.textContent).toContain('Welcome to React')
-    })
-  })
-})
+  test('should render as expected', () => {
+    const wrapper = shallow(<App />);
+    console.info(wrapper.debug());
+  });
+});
+s;
