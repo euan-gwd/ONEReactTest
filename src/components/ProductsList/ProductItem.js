1ae6dc4bcbdabde1ef046816332d424b549597aa
Euan Greenwood <euan@euangreenwood.com>
2020-03-06 13:56:37 +0000

    Add Modal

--- src/components/ProductsList/ProductItem.js
+++ src/components/ProductsList/ProductItem.js
@@ -2,30 +2,45 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { addToCart } from '../../store/actions';
 import Button from '../Buttons/Button';
+import Modal from 'react-modal';
 import './product-item-styles.css';
 
-const ProductItem = ({ product, addToCart }) => (
-  <div key={product.id} className="product-card">
-    <div className="image-container">
-      <img src={product.images[0].src} alt="product Image" className="product-image" />
-    </div>
-    <div className="card-content">
-      <div className="description">
-        <p className="title">{product.title}</p>
-        <p>{`£ ${product.price}`}</p>
+const ProductItem = ({ product, addToCart }) => {
+  const [isOpen, setModalOpen] = useState(false);
+  return (
+    <div key={product.id} className="product-card">
+      <div className="image-container">
+        <img src={product.images[0].src} alt="product Image" className="product-image" />
       </div>
-      <p className="size">{product.option1}</p>
+      <div className="card-content">
+        <div className="description">
+          <p className="title">{product.title}</p>
+          <p>{`£ ${product.price}`}</p>
+        </div>
+        <p className="size">{product.option1}</p>
+      </div>
+      <div className="button-group">
+        <Button className="primary-button" onClick={() => addToCart(product)}>
+          Add to Cart
+        </Button>
+        <Button className="secondary-button" onClick={() => setModalOpen(true)}>
+          Quick View
+        </Button>
+      </div>
+      <Modal isOpen={isOpen} className="product-modal" onRequestClose={() => setModalOpen(false)}>
+        <div className="product-modal-body">
+          <div className="image-container">
+            <img src={product.images[0].src} alt="product Image" className="product-image" />
+          </div>
+          <div className="modal-description">
+            <p>{product.title}</p>
+            <p>{product.option1}</p>
+          </div>
+        </div>
+      </Modal>
     </div>
-    <div className="button-group">
-      <Button className="primary-button" onClick={() => addToCart(product)}>
-        Add to Cart
-      </Button>
-      <Button className="secondary-button" disabled>
-        Quick View
-      </Button>
-    </div>
-  </div>
-);
+  );
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
+import Modal from 'react-modal';
 import App from './components/App';
 import './index.css';
 
 const store = configureStore();
+
+Modal.setAppElement('#app');
 
 render(
   <Provider store={store}>
